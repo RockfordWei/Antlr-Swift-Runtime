69c69f5703217c7dd3beb69577663a7691da44bf
John Pope <jp@fieldstormapp.com>
2017-05-01 22:27:30 -0400

    ⛳️ add golang walker..

--- Test/Test/AppDelegate.swift
+++ Test/Test/AppDelegate.swift
@@ -8,8 +8,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var window: NSWindow!
  
     
+    
     @IBAction func runHelloWalker(_ sender: AnyObject) {
         do {
+            
+            
+            
+            
+           
+            
+            //            let lexer = GolangLexer(ANTLRInputStream(input))
+            //            let tokens = CommonTokenStream(lexer)
+            //            let parser = try GolangParser(tokens)
+            //            let tree = try parser.sourceFile()
+            //            return tree.toStringTree(parser)
+            //
+            // if you get can not open / drag file to copy to bundle resources in build phases
+            let textFileName = "session.go"
+            
+            if let textFilePath = Bundle.main.path(forResource: textFileName, ofType: nil) {
+                let lexer =  GolangLexer(ANTLRFileStream(textFilePath))
+                print("lexer:",lexer)
+                let tokens =  CommonTokenStream(lexer)
+                let parser = try GolangParser(tokens)
+                
+                let tree = try parser.sourceFile()
+                print("tree:",tree.toStringTree(parser))
+                
+                let walker = ParseTreeWalker()
+                let golangwalker = GolangWalker()
+                try walker.walk(golangwalker,tree)
+                
+            } else {
+                print("error occur: can not open \(textFileName)")
+            }
+            
+        }catch ANTLRException.cannotInvokeStartRule {
+            print("error occur: CannotInvokeStartRule")
+        }catch ANTLRException.recognition(let e )   {
+            print("error occur\(e)")
+        }catch {
+            print("error occur")
+        }
+    }
+    
+    
+    @IBAction func runJavaHelloWalker(_ sender: AnyObject) {
+        do {
+
+            
+            
 
             // if you get can not open / drag file to copy to bundle resources in build phases
             let textFileName = "Test.java"
